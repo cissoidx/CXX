8724f508df47f1145e5c655864463b7e36d766d7
dong xu <xu.dong.gx@renesas.com>
2024-08-16 17:54:55 +0800

    update int to long

--- 3_mem_manage/pass_by/stack_usage.cpp
+++ 3_mem_manage/pass_by/stack_usage.cpp
@@ -1,31 +1,31 @@
 #include <stdio.h>
     
-void CallByValue(int i)
+void CallByValue(long i)
 {
-    int j = 1; 
+    long j = 1; 
     printf ("call-by-value: \t\t%p\n",&j);
 }
 
-void CallByPointer(int *i)
+void CallByPolonger(long *i)
 {
-    int j = 1; 
-    printf ("call-by-pointer: \t%p\n",&j);
+    long j = 1; 
+    printf ("call-by-polonger: \t%p\n",&j);
 }
 
-void CallByReference(int &i)
+void CallByReference(long &i)
 {
-    int j = 1; 
+    long j = 1; 
     printf ("call-by-reference: \t%p\n",&j);
 }
 
 int main()
 {
-    int i = 0;
+    long i = 0;
     printf ("stack bottom: \t\t%p\n",&i);
     
     CallByValue(i);
 
-    CallByPointer(&i);
+    CallByPolonger(&i);
 
     CallByReference(i);
 
